89322cbf337009036a2899c732a2864ff94436d4
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-22 00:19:34 +0530

    deleted unwanted things

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ export const requireSignIn = async (req, res, next) => {
   try {
     const decode = JWT.verify(
       req.headers.authorization,
-      process.env.JWT_SECRETssss
+      process.env.JWT_SECRET
     );
 
     req.user = decode;
