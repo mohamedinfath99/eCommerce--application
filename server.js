46ea82bfd5e1f4baed6ce85b9a14cc810f848460
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-21 22:15:32 +0530

    auth route added

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import colors from 'colors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import connect from './config/db.js';
+import authRoutes from './routes/authRoutes.js'
+
+
+
+// dotenv config
+dotenv.config();
+
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+// routes
+app.use('/api/v1/auth', authRoutes)
+
+
+//listen port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    connect();
+    console.log(`Server is running ${process.env.NODE_MODE} mode on port ${process.env.PORT}`.bgYellow.black);
+})
