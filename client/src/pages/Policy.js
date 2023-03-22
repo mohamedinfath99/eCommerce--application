fb4def10674b11938a11cb13ff2727551d580b83
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-22 16:01:44 +0530

    image added

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Layout from "./../components/Layout";
+import Layout from "../components/Layout/Layout";
+
 
 const Policy = () => {
   return (
     <Layout title={"Privacy Policy"}>
+
       <div className="row contactus ">
+
         <div className="col-md-6 ">
           <img
             src="https://assets1.chainstoreage.com/styles/primary_articles_short/s3/2020-06/ecommerce2_0.jpg?itok=oPvSXVwT"
@@ -12,6 +15,7 @@ const Policy = () => {
             style={{ width: "100%" }}
           />
         </div>
+
         <div className="col-md-4">
           <p>✒️ We offer free shipping on all orders within the United States.</p>
           <p>✒️ We collect only the necessary information to process and fulfill your orders.</p>
@@ -21,7 +25,9 @@ const Policy = () => {
           <p>✒️ You can contact us via email, phone, or live chat.</p>
           <p>✒️ We offer a warranty on all products against defects in materials and workmanship</p>
         </div>
+
       </div>
+
     </Layout>
   );
 };
