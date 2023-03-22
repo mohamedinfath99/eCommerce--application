115f04d50ad051df4ef1755429b2d1b55f940d7e
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-22 15:46:28 +0530

    iimaged changed

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Layout from "./../components/Layout";
+import Layout from "../../src/components/Layout/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
 
 const Contact = () => {
   return (
     <Layout title={"Contact us"}>
+
       <div className="row contactus ">
 
         <div className="col-md-6 ">
@@ -22,18 +23,25 @@ const Contact = () => {
           <p className="text-justify mt-2">
             Thank you for your interest in our products. If you have any questions or queries regarding our products, please feel free to contact us anytime as we are available 24X7 to assist you.
           </p>
+
           <br />
+
           <p className="mt-3">
             <BiMailSend /> : trippy@gmail.com
           </p>
+
           <p className="mt-3">
             <BiPhoneCall /> : 012-3456789
           </p>
+
           <p className="mt-3">
             <BiSupport /> : 1800-0000-0000 (toll free)
           </p>
+
         </div>
+
       </div>
+
     </Layout>
   );
 };
