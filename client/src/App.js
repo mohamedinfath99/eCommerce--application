27db2fe2881fd0bf2b47d87293645979a8419c41
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-22 15:36:19 +0530

    user, admin dashborad routes created

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,18 @@ import Policy from "./pages/Policy";
 import PageNotFound from "./pages/PageNotFound";
 import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
+import Dashboard from './pages/user/Dashboard';
+import PrivateRoute from './components/Routes/Private';
+import ForgotPasssword from './pages/Auth/ForgotPasssword';
+import AdminRoute from './components/Routes/AdminRoute';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import CreateCategory from './pages/Admin/CreateCategory';
+import CreateProduct from './pages/Admin/CreateProduct';
+import UpdateProduct from './pages/Admin/UpdateProduct';
+import Products from './pages/Admin/Products';
+import Users from './pages/Admin/Users';
+import Orders from './pages/user/Orders';
+import Profile from './pages/user/Profile';
 
 
 
@@ -15,7 +27,26 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
+
+        <Route path='/dashboard' element={<PrivateRoute />}>
+          <Route path='user' element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
+
+
+        <Route path='/dashboard' element={<AdminRoute />}>
+          <Route path='admin' element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          {/* <Route path="admin/product/:slug" element={<UpdateProduct />} /> */}
+          {/* <Route path="admin/products" element={<Products />} /> */}
+          <Route path="admin/users" element={<Users />} />
+          {/* <Route path="admin/orders" element={<AdminOrders />} /> */}
+        </Route>
+
         <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPasssword />} />
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
