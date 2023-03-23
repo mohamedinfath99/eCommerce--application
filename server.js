fbc83f405bcc0ba682eeded06e91d52ab9de1ea6
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-23 16:37:21 +0530

    catgory, product route created

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import connect from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
 
 
@@ -22,7 +24,9 @@ app.use(morgan('dev'));
 
 
 // routes
-app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 
 //listen port
