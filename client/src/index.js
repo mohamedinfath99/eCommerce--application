6a27be4c77aea347aee917d8b2a8ef9c40ba6f4d
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-23 16:34:51 +0530

    Search Provided code added

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from './context/auth';
+import { SearchProvider } from './context/search'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthProvider>
+    <SearchProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SearchProvider>
+  </AuthProvider>
 );
 
