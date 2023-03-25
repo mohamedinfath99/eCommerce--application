896a2ed14d8e7a26e5c01af1353b030e20ee354c
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-25 23:24:38 +0530

    card heading added

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -17,11 +17,12 @@ const About = () => {
 
         <div className="col-md-4">
 
+        <h1 className="bg-dark p-2 text-white text-center">ABOUT</h1>
+          <br />
+
           <p className="text-justify mt-2">
             Welcome to Triipy, your ultimate destination for all your travel needs. We are an online retailer specializing in travel gear, accessories, and essentials. Our mission is to provide you with high-quality products that make your travel experience comfortable and hassle-free.
-            <br />
-            <br />
-            At Triipy, we understand the importance of seamless travel. That's why we've curated a wide range of products that cater to all your travel requirements, whether you're heading on a weekend getaway or a backpacking adventure. Our collection includes travel bags, luggage, backpacks, travel accessories, and more, all made with the finest materials and craftsmanship.
+            At Triipy, we understand the importance of seamless travel. That's why we've curated a wide range of products that cater to all your travel requirements, whether you're heading on a weekend getaway or a backpacking adventure. 
           </p>
 
         </div>
