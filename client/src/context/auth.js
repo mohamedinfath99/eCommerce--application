5f11bce50fc97c21ff132d263638f6ec1b3db05a
mohamedInfath <mohamedinfath99@gmail.com>
2023-04-04 07:37:54 +0530

    code re-arranged

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -11,24 +11,32 @@ const AuthProvider = ({ children }) => {
     token: "",
   });
 
-  //default axios
   axios.defaults.headers.common["Authorization"] = auth?.token;
 
   useEffect(() => {
+
     const data = localStorage.getItem("auth");
+
     if (data) {
+
       const parseData = JSON.parse(data);
+
       setAuth({
         ...auth,
         user: parseData.user,
-        token: parseData.token,
+        token: parseData.token
+
       });
     }
+
   }, []);
+
   return (
+
     <AuthContext.Provider value={[auth, setAuth]}>
       {children}
     </AuthContext.Provider>
+
   );
 };
 
